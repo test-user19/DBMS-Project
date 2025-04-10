7e1672a54c3fde74e1c7e976533057a473915571
test-user19 <datatransfer7110@gmail.com>
2025-04-10 23:03:58 +0530

    support production

--- database_operations.py
+++ database_operations.py
@@ -1,16 +1,17 @@
 import mysql.connector
-from db_config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+from db_config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, PORT
 import json  # Import the json library
 
 def get_connection():
-    return mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+    return mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME,port=PORT)
 
 def create_database_if_not_exists():
     try:
         mydb = mysql.connector.connect(
             host=DB_HOST,
             user=DB_USER,
-            password=DB_PASSWORD
+            password=DB_PASSWORD,
+            port=PORT
         )
         mycursor = mydb.cursor()
         mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
--- db_config.py
+++ db_config.py
@@ -1,4 +1,6 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_NAME = "student_tracker"
+import os
+DB_HOST = os.getenv('DB_HOST','localhost')
+DB_USER = os.getenv('DB_USER','root')
+DB_PASSWORD = os.getenv('DB_PASSWORD','')
+DB_NAME = os.getenv('DB_NAME','student_tracker')
+PORT=os.getenv('PORT', '3306' )
